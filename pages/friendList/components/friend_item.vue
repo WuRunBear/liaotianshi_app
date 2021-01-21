866182f635123eb14a08265f66ba356cec1154d0
WuRunBear <1610945895@qq.com>
2021-01-21 20:22:41 +0800

    修改

---
+++ pages/friendList/components/friend_item.vue
@@ -0,0 +1,94 @@
+<template>
+  <view>
+    <view
+      @touchstart="friend_item_back_show"
+      @touchcancel="friend_item_back_hidden"
+      @touchend="friend_item_back_hidden"
+      :class="{ friend_item_back }"
+      class="friend_item"
+      v-if="info"
+    >
+      <view class="left">
+        <u-image
+          class="picture"
+          width="80rpx"
+          height="80rpx"
+          :src="info.picture"
+        ></u-image>
+      </view>
+
+      <view class="right">
+        <view class="title">{{ info.title }}</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: () => null,
+    },
+  },
+  name: 'friend_item',
+  data() {
+    return {
+      info: this.data,
+      friend_item_back: false,
+    };
+  },
+  methods: {
+    /**
+     * 被按下时背景颜色显示
+     */
+    friend_item_back_show() {
+      this.friend_item_back = true;
+    },
+    /**
+     * 放开时背景颜色隐藏
+     */
+    friend_item_back_hidden() {
+      this.friend_item_back = false;
+    },
+    /**
+     * 跳转
+     */
+    toBVue(to) {
+      uni.navigateTo({
+        url: to.path,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.friend_item_back {
+  background-color: #efefee;
+}
+.friend_item {
+  display: flex;
+  padding: 0 32rpx;
+  height: 110rpx;
+  font-family: PingFangSC-Regular;
+  border-bottom: 1rpx #e1e1e1 solid;
+
+  .left {
+    // width: 96rpx;
+    margin: 32rpx 32rpx 32rpx 0;
+    display: flex;
+    align-items: center;
+  }
+
+  .right {
+    flex: 1;
+    .title {
+      font-size: 28rpx;
+      color: #272832;
+      line-height: 110rpx;
+    }
+  }
+}
+</style>
--- pages/friendList/friendList.vue
+++ pages/friendList/friendList.vue
@@ -1,23 +1,115 @@
 <template>
   <view>
-    通讯录
+    <!-- 其他 -->
+    <view class="more">
+        <friend-item
+          :data='{
+            title: "新的好友"
+          }'
+        />
 
-    <u-button type="default" @click="addMsgCount">add</u-button>
+        <friend-item
+          :data='{
+            title: "群"
+          }'
+        />
+    </view>
+    <!-- 间隔槽 -->
+    <u-gap height="50" bg-color="#cfcfcf"></u-gap>
+    <!-- 好友列表 -->
+    <view class="friend_list">
+      <u-index-list :stick='false'>
+        <view
+          v-for="(item, index) in friendList"
+          :key='index'
+        >
+          <u-index-anchor :index="item.key" />
+          <friend-item
+            v-for="(child, i) in item.childs"
+            :key='i'
+            :data='child'
+          />
+        </view>
+      </u-index-list>
+    </view>
+
+    <!-- 右上角的菜单 -->
+    <more ref="more" />
   </view>
 </template>
 
 <script>
+import friendItem from './components/friend_item.vue'
 export default {
   name: 'friendList',
+  components: {
+    friendItem,
+  },
   data() {
-    return {};
+    return {
+      friendList: [
+        {
+          key: 'A',
+          childs: [
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+            {
+              title: "测试"
+            },
+          ]
+        }
+      ]
+    };
   },
   methods: {
-    addMsgCount() {
-      this.$store.commit('chatMessage/setNewMsgCount', 321);
+  },
+  /**
+   * 点击顶部右侧按钮
+   */
+  onNavigationBarButtonTap(e) {
+    if ("search" == e.types) {
+      this.$refs.more.$emit("hidden");
+    } else if ("more" == e.types) {
+      this.$refs.more.$emit("toggle");
     }
-  }
+  },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+  
+</style>
